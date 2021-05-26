a345b762a55b0b46adedadcbb5c4d5bf6b0526ae
UltraXPro <73195390+UltraXPro@users.noreply.github.com>
2021-05-26 08:43:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by OP Bots\nhttps://t.me/OPbots")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
