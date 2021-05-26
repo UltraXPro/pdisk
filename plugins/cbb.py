332ba715894814a149037a149be2c70b557d6487
UltraXPro <73195390+UltraXPro@users.noreply.github.com>
2021-05-26 08:55:52 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>⛑️ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n\n📝 Language : <code>Python3</code>\n\n🧰 Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n\n📖 Source Code : <a href='https://github.com/UltraXPro/pdisk'>Click here</a>\n\n📢 Channel : @StreamTVCommunity\n\n🎀 Support Group : @RequestStreamTV</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
